f949642968435953ead59722a5c585e5958a2e09
Stéphane <hery.stephane.rarivoson@gmail.com>
2022-07-20 15:10:04 +0300

    ajout de sidebar: non stylisé

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,12 +13,13 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+    {{-- sidebar --}}
+    <link rel="stylesheet" href="{{ asset('components/css/sidebars.css') }}">
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
@@ -79,6 +80,8 @@
             </div>
         </nav>
 
+        @include('layouts.sidebar')
+
         <main class="py-4">
             @yield('content')
         </main>
@@ -87,5 +90,9 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
 integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
+
+<script src="{{ asset('components/js/sidebars.js') }}"></script>
 
 </html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,13 @@
+<div class="d-flex flex-column flex-shrink-0 bg-light mt-5" style="width: 4.5rem;">
+
+    <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+      <li class="nav-item">
+        <a href="#" class="nav-link active py-3 border-bottom" aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
+            <i class='bx bx-calendar-minus fs-3'></i>
+        </a>
+      </li>
+      <li>
+
+    </ul>
+
+  </div>
