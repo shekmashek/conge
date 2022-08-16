dd3b563426d7fd901a793caffa763af0648e68b1
Stéphane <hery.stephane.rarivoson@gmail.com>
2022-08-16 10:27:38 +0300

    pitites rectif sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,9 @@
 
-<div class="d-flex flex-column flex-shrink-0 bg-light mt-5" style="width: 4.5rem;">
+<div class="d-flex flex-column flex-shrink-0 bg-transparent mt-5" style="width: 4.5rem;">
 
     <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
       <li class="nav-item">
-        <a href="#" class="nav-link active py-3 border-bottom" aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
+        <a href="#" class="active border-bottom ms-1 nav-link py-3 rounded-3" aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
             <i class='bx bx-calendar-minus fs-3'></i>
         </a>
       </li>
