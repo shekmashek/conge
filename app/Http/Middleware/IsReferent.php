a0cf4326e9dd7d170b9dd2b6db658fe6cd7969bc
Stéphane <hery.stephane.rarivoson@gmail.com>
2022-09-08 15:39:53 +0300

    route

--- app/Http/Middleware/IsReferent.php
+++ app/Http/Middleware/IsReferent.php
@@ -23,15 +23,27 @@ class IsReferent
 
         // utilisation de in_array() sur le tableau des roles de l'utilisateur connecté
         if (in_array('2', Auth::user()->roles->pluck('id')->toArray())) {
-            return $next($request);
+
+            if (Auth::user()->roles->where('id', 2)->first()->pivot->activiter == 1) {
+                return $next($request);
+            } else if (Auth::user()->roles->where('id', 3)->first()->pivot->activiter == 1) {
+                return redirect()->route('conge_employe')->with('info', 'Vous êtes connecté en tant qu\'employe');
+            } else if (Auth::user()->roles->where('id', 9)->first()->pivot->activiter == 1) {
+                return redirect()->route('home_RH')->with('info', 'Vous êtes connecté en tant qu\'RH');
+            } else if (Auth::user()->roles->where('id', 5)->first()->pivot->activiter == 1) {
+                return redirect()->route('home_manager')->with('info', 'Vous êtes connecté en tant que manager');
+            } else {
+                return redirect()->route('sign-in');
+            }
+
         }
 
-        if (!in_array('2', Auth::user()->roles->pluck('id')->toArray())) {
-            return redirect()->back()->with('error', 'Vous n\'avez pas les droits pour accéder à cette page');
-        }
-        // utilisation de contains() sur un objet
-        if (Auth::user()->roles->pluck('id')->contains('3')) {
-            return redirect()->route('home')->with('error', 'Vous n\'avez pas les droits pour accéder à cette page');
-        }
+        // if (!in_array('2', Auth::user()->roles->pluck('id')->toArray())) {
+        //     return redirect()->back()->with('error', 'Vous n\'avez pas les droits pour accéder à cette page');
+        // }
+        // // utilisation de contains() sur un objet
+        // if (Auth::user()->roles->pluck('id')->contains('3')) {
+        //     return redirect()->route('home')->with('error', 'Vous n\'avez pas les droits pour accéder à cette page');
+        // }
     }
 }
