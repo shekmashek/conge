16f2be9caeb4fbb40c8c7973bf518656855f2bae
Stéphane <hery.stephane.rarivoson@gmail.com>
2022-09-08 09:14:13 +0300

    modificatiuon nom table employes->employers

--- app/Models/Employe.php
+++ app/Models/Employe.php
@@ -16,7 +16,8 @@ class Employe extends Model
 {
     use HasFactory;
 
-    protected $table = 'employes';
+    // protected $table = 'employes';
+    protected $table = 'employers';
 
 
     public function entreprise(): BelongsTo
