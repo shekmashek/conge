48e394e75f29c5c27fbca5273c4a08fe5bde2b52
Laih0 <stvrzf2k16@gmail.com>
2022-09-08 11:53:10 +0300

    liste employes rh fini

--- app/Http/Controllers/RHController.php
+++ app/Http/Controllers/RHController.php
@@ -99,7 +99,72 @@ class RHController extends Controller
 
     }
 
-    //-----------liste employer depuis l'interface RH--------------------------------------------------
+    //-----------liste employer V1 depuis l'interface RH --------------------------------------------------
+
+    // public function liste_employes(Request $request)
+    // {
+    //    $authed_RH = auth()->user()->employe;
+
+
+    //         $employes=Employe::with('service','service.departement','entreprise','contrat')
+    //         ->where('entreprise_id', $authed_RH->entreprise_id)
+    //         ->where('id', '!=', $authed_RH->id)
+    //         ->get(['id', 'nom_emp', 'prenom_emp', 'email_emp', 'telephone_emp', 'service_id', 'entreprise_id', 'photos']);
+
+
+
+    //     //  dd($employes->pluck('contrat'));
+
+
+    //     if ($request->ajax()) {
+    //         $employes=Employe::with('service','service.departement','entreprise','contrat')
+    //         ->where('entreprise_id', $authed_RH->entreprise_id)
+    //         ->where('id', '!=', $authed_RH->id)
+    //         ->get(['id', 'nom_emp', 'prenom_emp', 'email_emp', 'telephone_emp', 'service_id', 'entreprise_id', 'photos']);
+
+    //         $employes = DataTables::of($employes)
+    //             ->addColumn('nom_prenom', function($s){
+    //                 // show the photo of the employe
+    //                 // <img src="'.asset('storage/'.$s->photos).'" class="img-fluid" alt="">
+    //                 // <img src="'.$s->photos.'" class="img-fluid" alt="">
+    //                 $r = '<div class="media align-items-center">
+    //                             <div class="media-left">
+    //                                 <img src="'.asset('img/users/'.$s->photos).'" class="img-fluid" alt="">
+    //                             </div>
+    //                             <div class="media-body">
+    //                                 <h4 class="mb-0">'.$s->nom_emp.' '.$s->prenom_emp.'</h4>
+    //                                 <span>'.$s->email_emp.'</span>
+    //                             </div>
+    //                         </div>';
+    //                 return $r;
+    //             })
+
+    //             // ->addColumn('actions', function($s){
+    //             //     $r = '<div  class="dropdown dropstart myDrop" data-conge-id="'.$s->id.'">
+    //             //                 <button class="btn fs-3" type="button" id="action_button" data-bs-toggle="dropdown" aria-expanded="false">
+    //             //                 <i class="bx bx-dots-vertical-rounded"></i>
+    //             //                 </button>
+    //             //                 <ul class="dropdown-menu dropdown-start" aria-labelledby="etat_actions">
+    //             //                     <li><button  class="dropdown-item btnAccepter" type="button" >Voir</button></li>
+    //             //                     <li><button  class="dropdown-item btnRefuser" type="button" onclick="">Historique</button></li>
+    //             //                 </ul>
+    //             //             </div>';
+    //             //     return $r;
+    //             // })
+    //             // ->rawColumns(['nom_prenom', 'actions'])
+    //             ->rawColumns(['nom_prenom'])
+    //             ->make(true);
+
+
+
+    //         return $employes;
+    //     }
+
+    //     return view('rh.liste_employes');
+
+    // }
+
+    //-----------liste employer V2 depuis l'interface RH --------------------------------------------------
 
     public function liste_employes(Request $request)
     {
@@ -109,34 +174,37 @@ class RHController extends Controller
             $employes=Employe::with('service','service.departement','entreprise','contrat')
             ->where('entreprise_id', $authed_RH->entreprise_id)
             ->where('id', '!=', $authed_RH->id)
-            ->get(['id', 'nom_emp', 'prenom_emp', 'email_emp', 'telephone_emp', 'service_id', 'entreprise_id']);
-
-
-
-        //  dd($employes->pluck('contrat'));
+            ->get(['id', 'nom_emp', 'prenom_emp', 'email_emp', 'telephone_emp', 'service_id', 'entreprise_id', 'photos']);
 
 
         if ($request->ajax()) {
             $employes=Employe::with('service','service.departement','entreprise','contrat')
             ->where('entreprise_id', $authed_RH->entreprise_id)
             ->where('id', '!=', $authed_RH->id)
-            ->get(['id', 'nom_emp', 'prenom_emp', 'email_emp', 'telephone_emp', 'service_id', 'entreprise_id']);
+            ->get(['id', 'nom_emp', 'prenom_emp', 'email_emp', 'telephone_emp', 'service_id', 'entreprise_id', 'photos']);
 
             $employes = DataTables::of($employes)
+                ->addColumn('id', function($s){
+                    return $s->id;
+                })
+
                 ->addColumn('nom_prenom', function($s){
                     // show the photo of the employe
                     // <img src="'.asset('storage/'.$s->photos).'" class="img-fluid" alt="">
-                    $r = '<div class="media align-items-center">
-                                <div class="media-left">
-                                    <img src="'.$s->url_photo.'" class="img-fluid" alt="">
-                                </div>
-                                <div class="media-body">
-                                    <h4 class="mb-0">'.$s->nom_emp.' '.$s->prenom_emp.'</h4>
-                                    <span>'.$s->email_emp.'</span>
-                                </div>
-                            </div>';
+                    // <img src="'.$s->photos.'" class="img-fluid" alt="">
+                    $r = '<span class="align-items-center d-flex justify-content-around" style="width: 250px">
+                                <img src="'.asset('img/users/'.$s->photos).'" class="img-fluid rounded-circle" alt style="width: 70px;height: 70px;object-fit: cover;">
+                                <span class="ms-2">'.$s->nom_emp.' '.$s->prenom_emp.'</span>
+                            </span>';
+
                     return $r;
                 })
+
+                ->addColumn('contacts', function($s){
+                    $r = '<span class="ms-2 ">'.$s->email_emp. '<br>'.$s->telephone_emp. '</span>';
+                    return $r;
+                })
+
 
                 // ->addColumn('actions', function($s){
                 //     $r = '<div  class="dropdown dropstart myDrop" data-conge-id="'.$s->id.'">
@@ -151,7 +219,7 @@ class RHController extends Controller
                 //     return $r;
                 // })
                 // ->rawColumns(['nom_prenom', 'actions'])
-                ->rawColumns(['nom_prenom'])
+                ->rawColumns(['nom_prenom', 'contacts'])
                 ->make(true);
 
 
@@ -161,8 +229,8 @@ class RHController extends Controller
 
         return view('rh.liste_employes');
 
-    }
 
+    }
 
 
 
--- resources/views/rh/liste_employes.blade.php
+++ resources/views/rh/liste_employes.blade.php
@@ -15,23 +15,22 @@
 @endpush
 
 @section('content')
-<div class="container mt-5">
-    <table id="liste_employe_RH" class="table" style="width:100%">
-        <thead>
-            <tr>
-                <th>Nom Prenom(s)</th>
-                <th>Département/Service</th>
-                <th>Date d'embauche</th>
-                {{-- <th>Actions</th> --}}
-            </tr>
-        </thead>
-        <tbody>
-
-
-        </tbody>
-
-    </table>
-
+<div class="container">
+            <div class="row">
+                <div class="fixedTop mt-2">
+                    <table class="table" id="liste_employe_RH">
+                        <thead style="background: #c7c9c939">
+                            <tr>
+                                <th class="align-middle text-center">ID</th>
+                                <th class="align-middle text-center">Employé</th>
+                                <th class="align-middle text-center">Contacts</th>
+                                <th class="align-middle text-center">Département <br> Service</th>
+                                <th class="align-middle text-center">Date d'embauche</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
 </div>
 @endsection
 
@@ -63,20 +62,36 @@
             },
             columns: [
                 {
+                    data: 'id', name: 'id'
+                    , className: 'align-middle text-center'
+                },
+
+                {
                     data: 'nom_prenom',
+                    className: 'align-middle text-center'
+
                 },
                 {
+                    data: 'contacts',
+                    className: 'align-middle text-center'
+
+                },
+
+                {
                     data: 'service.departement.nom_departement',
+                    className: 'align-middle text-center',
                     render: function (data, type, row) {
-                        return row.service.departement.nom_departement + ' / ' + row.service.nom_service;
+                        return row.service.departement.nom_departement + ' <br> ' + row.service.nom_service;
                     }
                 },
                 {
                     data: 'contrat.date_embauche',
+                    className: 'align-middle text-center',
                     render: function (data, type, row) {
                         // tranfrom date to dd/mm/yyyy
                         var date = new Date(row.contrat.date_embauche);
                         return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+                        className: 'align-middle text-center'
                     }
                 },
                 // {
@@ -99,11 +114,16 @@
                     "visible": true,
                     "searchable": true
                 },
-                // {
-                //     "targets": [ 3 ],
-                //     "visible": true,
-                //     "searchable": false
-                // },
+                {
+                    "targets": [ 3 ],
+                    "visible": true,
+                    "searchable": true
+                },
+                {
+                    "targets": [ 4 ],
+                    "visible": true,
+                    "searchable": true
+                },
             ]
         });
         new $.fn.dataTable.FixedHeader( table );
