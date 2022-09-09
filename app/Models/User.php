30e6d8f10d3688ad7534d1f1843ff187d0fa14f8
Stéphane <hery.stephane.rarivoson@gmail.com>
2022-09-09 16:02:55 +0300

    roles users no created_at

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,7 +50,7 @@ class User extends Authenticatable
 
     public function roles()
     {
-        return $this->belongsToMany(Role::class, 'role_users')->withPivot('activiter', 'created_at', 'updated_at');
+        return $this->belongsToMany(Role::class, 'role_users')->withPivot('activiter');
     }
 
     public function employe()
